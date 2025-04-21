98c48c5f5fbffde1fbbc21a9a2bc5d5e990924f5
Ivan Savchuk <116903781+IvanSavchuk7@users.noreply.github.com>
2025-04-21 16:53:04 +0300

    Add files via upload

---
+++ js/start.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const iframe = document.getElementById("video-frame");
+    const playOverlay = document.querySelector(".video-container .play");
+    const playBtn = document.querySelector(".play-btn");
+
+    function playVideo() {
+        // Start YouTube video
+        iframe.contentWindow.postMessage(
+            JSON.stringify({
+                event: "command",
+                func: "playVideo",
+                args: []
+            }),
+            "*"
+        );
+
+        iframe.classList.add("normal-size");
+        setTimeout(() => {
+            playOverlay.style.display = "none";
+        }, 200);
+
+        if (window.innerWidth > 767) {
+            document.getElementById("video-target").scrollIntoView({
+                behavior: "smooth",
+                block: "center"
+            });
+        }
+
+    }
+
+    playOverlay.addEventListener("click", playVideo);
+    playBtn.addEventListener("click", function (e) {
+        e.preventDefault();
+        playVideo();
+    });
+});
